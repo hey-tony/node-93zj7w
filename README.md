0d0a94e70ddc11bb2fa530d12a8a2175ac786cd0
hey-tony <910352+hey-tony@users.noreply.github.com>
2022-11-08 11:13:54 -0500

    Updating README.md

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
-# node-93zj7w
+# XLSX Merge Cell Test
+To run type `sh start.sh` in the jsh terminal below. Handles npm package installation as StackBlitz doesn't perserve node_modules on reload.
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-93zj7w)
+Use Ctrl-C to stop and if you want to run again after stopping:
+
+```
+cd ~/projects/node-93zj7w/backend
+npm run serve
+```
+
+if you made changes to my code and want to see the changes:
+
+```
+cd ~/projects/node-93zj7w/backend
+npm run start
+```
+
+To run CLI portion:
+
+```
+cd ~/projects/node-93zj7w
+sh cli.sh 
+```
+
+## /backend/lib/xlsx_parser 
+This is a npm module that I wrote that will take in an XLSX file stream and return all merged cells within the worksheet, including other information.
+
+This was meant to be a reusable package consumable by both Hapi server and CLI app. I wrote it to use Node pipes so that adding extract features/ functionality would be very simple. (i.e, file validation)
+
+## /backend/app
+The CLI portion of the test.
+
+## /backend/server and /frontend
+Just a simple frontend and Hapi server to test the `xlsx_parser`.
+
+## Thank you for taking the time!
+-Anthony
+
+[Created using StackBlitz ⚡️](https://stackblitz.com/edit/node-93zj7w)
--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -1,3 +1,45 @@
 import XlsxParser from '../../lib/xlsx_parser/dist';
+const axios = require('axios');
 
-console.log('Hello World');
+async function main() {
+    const sampleFile = 'https://raw.githubusercontent.com/hey-tony/node-93zj7w/main/sample/sample.xlsx';
+    
+    const response = await axios({
+        method: 'get',
+        url: sampleFile,
+        responseType: 'stream'
+    });
+    const stream = response.data;
+    const { sheets, sharedStrings } = await new XlsxParser().parse( stream );
+    
+    sheets.forEach( sheet => {
+        let { mergedCells } = sheet;
+        mergedCells = mergedCells.map( cell => {
+            const sharedString = 'ss';
+            const { content } = cell;
+            const isString = typeof content === 'string';
+            const isSharedString = isString && content.startsWith( sharedString );
+
+            if ( isSharedString ) {
+                const delimiter = '|';
+                const indexPos = 1;
+                const stringIndex = parseInt( content.split( delimiter )[ indexPos ] );
+                cell.content = sharedStrings[ stringIndex ];
+            }
+            return cell;
+        });
+        console.log(`\n\nWorksheet: ${sheet.name}`);
+        console.log('-------------------------\n');
+        console.log('  Merged cells:')
+        for ( let cell of mergedCells ) {
+            console.log();
+            console.log(`    >> Range: ${cell.range.text}`)
+            console.log( `       Content: ${cell.content || 'empty'}`);
+            console.log( `       Height: ${cell.height || 'default'}`);
+            console.log( `       Width: ${cell.width || 'default'}`);
+        }
+    });
+}
+
+main();
+
